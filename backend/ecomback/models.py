1bdcf17831aa1706eb33f458350cc8d039eabe19
housseynou coulibaly <housseynou7@gmail.com>
2023-07-26 14:14:43 +0200

    extracting objects from the backend working

--- backend/ecomback/models.py
+++ backend/ecomback/models.py
@@ -6,10 +6,10 @@ class Ecomback(models.Model):
         VET ='VETEMENT'
         CHAUS = 'CHAUSSURE'
 
-    name = models.fields.CharField(max_length=50)
-    description = models.fields.CharField(max_length=1000)
-    type = models.fields.CharField(choices=Type.choices, max_length=10)
-    inStock = models.fields.BooleanField(default=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    type = models.CharField(choices=Type.choices, max_length=10)
+    inStock = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.name}'
--- backend/ecomback/views.py
+++ backend/ecomback/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from ecomback.models import Ecomback
+from .models import Ecomback
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import generics
 
-def product_list(request):
-    products = Ecomback.objects.all()
-    return render(request, 'ecomback/product_list.html', {'products': products})
+class ProductView(generics.ListAPIView):
+    queryset = Ecomback.objects.all()
+    serializer_class = ProductSerializer
+#class ReactView(APIView):   
+ #   def get(self, request):
+ #       product = [{'name': product.name, 'description': product.description, 'type': product.type, 'inStock': product.inStock}
+ #                   for product in Ecomback.objects.all()]
+ #       return Response(product)
+       #products = Ecomback.objects.all()
+       #for product in products:
+        #   return Response(product)
+    
+ #   def post(self, request):
+ #       serializers = Reactserializer(data=request.data)
+  #      if serializers.is_valid(raise_exception=True):
+ #           serializers.save()
+  #          return Response(serializers.data)
 
